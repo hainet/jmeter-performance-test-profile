7f8c5cf829f739e117fe00bc419586e778e3ea64
hainet <20400616+hainet@users.noreply.github.com>
2019-02-14 17:15:16 +0900

    必要なディレクトリを作成する記述を追加した。

--- bin/exec.ps1
+++ bin/exec.ps1
@@ -5,6 +5,8 @@ $TEMP_PROFILE="$PROFILE-$DATETIME"
 
 Copy-Item .\$PROFILE.jmx .\$TEMP_PROFILE.jmx
 
+New-Item log -ItemType Directory -ErrorAction SilentlyContinue
+New-Item report -ItemType Directory -ErrorAction SilentlyContinue
 Invoke-Expression "$JMETER_HOME\bin\jmeter.bat -n -t $TEMP_PROFILE.jmx -l .\log\$TEMP_PROFILE.log -e -o .\report\$TEMP_PROFILE"
 
-Remove-Item .\PerformanceTest-$DATETIME.jmx
+Remove-Item .\$TEMP_PROFILE.jmx
